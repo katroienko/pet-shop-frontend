76a9d93723614a08a10ad8ab4e524061bce01835
Roienko Kateryna <roienkokateryna@gmail.com>
2025-05-23 18:19:21 +0200

    add VITE_API_URL

--- src/api/backendInstance.js
+++ src/api/backendInstance.js
@@ -1,5 +1,15 @@
-import axios from "axios"
+// import axios from "axios"
+
+// export const backendInstance = axios.create({
+//     baseURL: "http://localhost:3333"
+// })
+
+import axios from "axios";
+
+const { VITE_API_URL } = import.meta.env
 
 export const backendInstance = axios.create({
-    baseURL: "http://localhost:3333"
-})
+    baseURL: VITE_API_URL
+});
+
+export const localUrl = VITE_API_URL;
