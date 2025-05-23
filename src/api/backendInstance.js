7e9af9e93b7caa4bd4bf38554d9e560898dc3369
Roienko Kateryna <roienkokateryna@gmail.com>
2025-05-23 18:32:24 +0200

    add log

--- src/api/backendInstance.js
+++ src/api/backendInstance.js
@@ -6,7 +6,9 @@
 
 import axios from "axios";
 
-const { VITE_API_URL } = import.meta.env
+const { VITE_API_URL } = import.meta.env;
+console.log(VITE_API_URL);
+
 
 export const backendInstance = axios.create({
     baseURL: VITE_API_URL
