3ba1d2a75649377d85af6e413573011c2715fa46
Roienko Kateryna <roienkokateryna@gmail.com>
2025-05-24 11:05:58 +0200

    fix footer contacts

---
+++ src/components/Footer/contacts.js
@@ -0,0 +1,11 @@
+const contacts = {
+  phone: '+49 30 915-88492',
+  address: 'Wallstraẞe 9-13, 10179 Berlin, Deutschland',
+  workingHours: '24 hours a day',
+  social: {
+    instagram: 'https://www.instagram.com/',
+    whatsapp: 'https://www.whatsapp.com/',
+  },
+};
+
+export default contacts;
